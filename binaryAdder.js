ae0bab9e9b4062f9a9b63a2adc9c2d05d5286938
ju-kkim <wndus8198@naver.com>
2022-01-04 21:52:35 +0900

    refactor : 바이트 덧셈기 입력값 길이 다른경우 대비 리팩토링링

--- binaryAdder.js
+++ binaryAdder.js
@@ -47,13 +47,26 @@ function fulladder(bitA, bitB, carry) {
 function byteadder(byteA, byteB) {
     var answer = [];
     let carry = false
+    const aLength = byteA.length;
+    const bLength = byteB.length;
+    const longLength = Math.max(aLength, bLength);
 
-    for(let idx = 0; idx <= byteA.length; idx++){
+    for(let idx = 0; idx < longLength; idx++){
+        if(idx === aLength) {
+            byteA.push(false)
+        }
+        if(idx === bLength) {
+            byteB.push(false)
+        }
         const calc = fulladder(byteA[idx], byteB[idx], carry);
         const sum = calc[1]
         answer.push(sum)
         carry = calc[0];
     }
-    
+    if(carry === true) {
+        answer.push(carry)
+    }
     return answer;
 }
+
+console.log(byteadder([true,true], [false,false, true]))
